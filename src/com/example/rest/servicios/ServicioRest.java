96b78fcc9ce767b9dfd4f50dc40c284d080df8fb
Usuario <Usuario@ANDRESYG>
2020-07-01 16:28:50 -0500

    Se agrego el rest de tipoReclamo

--- src/com/example/rest/servicios/ServicioRest.java
+++ src/com/example/rest/servicios/ServicioRest.java
@@ -1,8 +1,12 @@
 package com.example.rest.servicios;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -12,6 +16,8 @@ import org.apache.commons.logging.LogFactory;
 
 import com.example.rest.dao.UsuarioModel;
 import com.example.rest.entidades.Usuario;
+import com.example.rest.entidades.TipoDeReclamo;
+import com.example.rest.dao.TipoDeReclamoModel;
 
 @Path("/servicios")
 @Produces({ MediaType.APPLICATION_JSON })
@@ -19,6 +25,7 @@ import com.example.rest.entidades.Usuario;
 public class ServicioRest {
 	private static final Log log = LogFactory.getLog(ServicioRest.class);
 	private UsuarioModel daoUser = new UsuarioModel();
+	private TipoDeReclamoModel daoTipo = new TipoDeReclamoModel();
 
 	@GET
 	@Path("/login")
@@ -34,6 +41,41 @@ public class ServicioRest {
 		return Response.ok(daoUser.listarTodos()).build();
 	}
 
-	
+	// Crud de tipoReclamo
+	@GET
+	@Path("/tiporeclamo")
+	public Response listartipoReclamoTodos() {
+		log.info("listars tiporeclamo rest ");
+		return Response.ok(daoTipo.listartipoReclamoTodos()).build();
+	}
 
+	@POST
+	@Path("/tiporeclamo")
+	public Response registratipoReclamo(TipoDeReclamo obj) {
+		log.info("Registra rol " + obj.getIdTipo());
+		if (daoTipo.insertatipoReclamo(obj) > 0)
+			return Response.ok().build();
+		else
+			return Response.notModified().build();
+	}
+
+	@PUT
+	@Path("/tiporeclamo")
+	public Response atualizatipoReclamo(TipoDeReclamo obj) {
+		log.info("Actualiza rol " + obj.getIdTipo());
+		if (daoTipo.actualizatipoReclamo(obj) > 0)
+			return Response.ok().build();
+		else
+			return Response.notModified().build();
+	}
+
+	@DELETE
+	@Path("/tiporeclamo/{idTipo}")
+	public Response eliminatipoReclamo(@PathParam("idTipo") int id) {
+		log.info("Elimina tiporeclamo " + id);
+		if (daoTipo.eliminatipoReclamo(id) > 0)
+			return Response.ok().build();
+		else
+			return Response.notModified().build();
+	}
 }
